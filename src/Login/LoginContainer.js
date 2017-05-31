c4f75bcb68212f4c3473aed8210b38e14931bdc6
Yzhang1994 <yzhang1994@gmail.com>
2017-05-31 10:43:01 -0700

    add top-level stack navigator and facebook login

---
+++ src/Login/LoginContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+// import setUserName from './homePageActions';
+import { connect } from 'react-redux';
+import { Button, StyleSheet, Text, TextInput, View, Image, TouchableHighlight} from 'react-native';
+import FBLogin from './FBLogin';
+
+class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSkip = this.handleSkip.bind(this);
+  }
+
+  handleSkip(e) {
+    const { navigate } = this.props.navigation;
+    navigate('HomePage');
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+        <Button title="Skip Login" onPress={this.handleSkip} />
+        <FBLogin />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { username } = state;
+  return {
+    //username: username
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // greetUser: () => {
+    //   dispatch(setUserName());
+    // }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
