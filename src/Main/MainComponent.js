91b6a3de42284a646e31f76586ff03033deb57ea
yzhang1994 <yzhang1994@gmail.com>
2017-06-01 15:00:00 -0700

    Revert "Add React Native Google Maps functionality"

--- src/Main/MainComponent.js
+++ src/Main/MainComponent.js
@@ -6,12 +6,7 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = 0.0421;
+import MapView from 'react-native-maps';
 
 class Main extends Component {
   constructor(props) {
@@ -32,51 +27,33 @@ class Main extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: 'green', height: 100, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Some div</Text>
-        </View>
-        <View style={styles.container}>
-          <MapView
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            initialRegion={{
-              latitude: LATITUDE,
-              longitude: LONGITUDE,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }}
-          />
-        </View>
+      <View style={styles.container}>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          onPress={this.greetUser}
+          title="submit name"
+          color="#841584"
+        />
+        <Text style={styles.instructions}>
+          Welcome to the Community, {this.props.username}!
+        </Text>
+        <Text style={styles.instructions}>
+          Create or find your ideal event,{'\n'}
+          where you can meet new friends and make memories of a lifetime!
+        </Text>
       </View>
     );
   }
-      // <View style={styles.container}>
-      //   <TextInput
-      //     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-      //     onChangeText={(text) => this.setState({text})}
-      //     value={this.state.text}
-      //   />
-      //   <Button
-      //     onPress={this.greetUser}
-      //     title="submit name"
-      //     color="#841584"
-      //   />
-      //   <Text style={styles.instructions}>
-      //     Welcome to the Community, {this.props.username}!
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     Create or find your ideal event,{'\n'}
-      //     where you can meet new friends and make memories of a lifetime!
-      //   </Text>
-      // </View>
 };
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -90,9 +67,6 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-  map: {
-     ...StyleSheet.absoluteFillObject,
-  }
 });
 
 export default Main;
