45f530656debb9deb6ff8570a69226e110847bf8
kvo180 <khoavo180@gmail.com>
2017-06-04 13:25:24 -0700

    Implement EventListComponent

---
+++ src/Main/EventList/EventListComponent.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  RefreshControl,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  View, Dimensions } from 'react-native';
+
+const styles = StyleSheet.create({
+  row: {
+    borderColor: 'grey',
+    borderWidth: 0,
+    marginVertical: 10,
+    marginHorizontal: 7
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#000',
+  },
+  scrollview: {
+    height: '100%',
+    width: '100%',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  }
+});
+
+class Row extends React.Component {
+
+  _onClick() {
+    console.log('clicked');
+  };
+
+  render() {
+    return (
+     <TouchableWithoutFeedback onPress={this._onClick.bind(this)} >
+        <View style={styles.row}>
+          <Image
+            style={styles.image}
+            source={{uri: this.props.data.image}}
+          />
+          <Text style={styles.text} >
+            {this.props.data.event_name}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+class EventListComponent extends Component {
+  // static title = '<RefreshControl>';
+  // static description = 'Adds pull-to-refresh support to a scrollview.';
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRefreshing: false,
+      loaded: 0
+    };
+    this._onClick = this._onClick.bind(this);
+    this._onRefresh = this._onRefresh.bind(this);
+  }
+
+   _onClick(row) {
+    row.clicks++;
+    this.setState({
+      rowData: this.state.rowData,
+    });
+  };
+
+  render() {
+
+    return (
+      <ScrollView
+        style={styles.scrollview}
+        // bounces={false}
+        // alwaysBounceVertical={false}
+        removeClippedSubViews={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._onRefresh}
+            tintColor="#ff0000"
+            title="Loading..."
+            titleColor="#00ff00"
+            colors={['#ff0000', '#00ff00', '#0000ff']}
+            progressBackgroundColor="#ffff00"
+          />
+        }>
+        {this.props.allEvents.map((row, index) => {
+          return <Row key={index} data={row}/>;
+        })}
+      </ScrollView>
+    );
+  }
+
+  _onRefresh() {
+    this.setState({isRefreshing: true});
+    setTimeout(() => {
+      // prepend 10 items
+      const rowData = ['newtest', 'newtest', 'newtest', 'newtest', 'newtest'];
+
+      this.setState({
+        loaded: this.state.loaded + 10,
+        isRefreshing: false,
+        rowData: rowData,
+      });
+    }, 5000);
+  };
+}
+
+// var createRow = ()
+
+export default EventListComponent;
--- src/Main/EventList/EventListContainer.js
+++ src/Main/EventList/EventListContainer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Button, StyleSheet, Text, TextInput, View, Image, TouchableHighlight} from 'react-native';
+import EventListComponent from './EventListComponent';
 
 class EventListContainer extends Component{
   static navigationOptions = ({ navigation, screenProps }) => {
@@ -15,9 +18,29 @@ class EventListContainer extends Component{
 
   render() {
     return (
-      <View><Text>EVENT LIST</Text></View>
+      <View>
+        <EventListComponent {...this.props}/>
+      </View>
     );
   }
 }
 
-export default EventListContainer;
+// const mapDispatchToProps = (dispatch) => {
+//   return bindActionCreators({
+//     centerLocation: centerLocation,
+//     addEvents: addEvents
+//   }, dispatch);
+// }
+
+// TODO: add mapDispatchToProps to export default below
+
+const mapStateToProps = (state) => {
+  const { mainReducer, mapReducer } = state;
+
+  return {
+    allEvents: mainReducer.allEvents,
+    coords: mapReducer.coords
+  };
+};
+
+export default connect(mapStateToProps)(EventListContainer);
