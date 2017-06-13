433112e3c244ff9e283e1e924b36dbd7666b405a
kvo180 <khoavo180@gmail.com>
2017-06-13 15:02:29 -0700

    Add ActivityIndicator overlay to CreateEventComponent

--- src/Main/CreateEvent/CreateEventComponent.js
+++ src/Main/CreateEvent/CreateEventComponent.js
@@ -78,6 +78,7 @@ class CreateEventComponent extends Component {
       return;
     }
 
+
     if (this.state.eventImageUri) {
 
       const imageId = this.state.eventImageUri.split('id=')[1].split('&ext=')[0];
